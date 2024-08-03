bc3211eb2f72fa56981d3f97672e2870313bc45c
nizam007ali <nizam007ali@gmail.com>
2024-08-04 01:15:49 +0530

    datatypes

---
+++ cpp/datatypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//5 types 
+	int number = 10;				
+	cout<<number<<endl;
+	double decimalNumber = 20.5;
+	cout<<decimalNumber<<endl;
+	char letter = 'n';
+	cout<<letter<<endl;
+	string Name = "Nizam";
+	cout<<Name<<endl;
+	bool Boolean = false;		//(true or false) or (1 or 0)
+	cout<<Boolean<<endl;
+}
+
