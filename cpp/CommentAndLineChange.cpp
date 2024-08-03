b0a552ad601fde9adca2bb7be56385c61000e9b2
nizam007ali <nizam007ali@gmail.com>
2024-08-04 00:52:08 +0530

    Comment and Line change

---
+++ cpp/CommentAndLineChange.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	//two ways to change line in c++
+	//Single-line comment
+	cout<<"Single-line comment and line change using \\n. \n";		//line change using \n
+	/*This is a multi-line
+	comment*/
+	cout<<"Multi-line comment and line change using endl."<<endl;	//line change using endl
+	cout<<"Here, we learn about comments and line change in c++.";									
+}
+
