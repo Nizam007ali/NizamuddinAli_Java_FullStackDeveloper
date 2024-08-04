854345d5668cf6a17e958bb5ec62d62a3ae5eaea
nizam007ali <nizam007ali@gmail.com>
2024-08-04 15:14:20 +0530

    Primitive datatype + string

--- java/PrimitiveDatatype.java
+++ java/PrimitiveDatatype.java
@@ -2,14 +2,14 @@ public class PrimitiveDatatype {
     public static void main(String args[])  
     {  
         String name = "Nizamuddin Ali";         //non-primitive
-        boolean isMale = true;
-        byte age = 24;
-        short salary = 32000;
-        int Age = 24;
-        long Salary = 35000;
-        float pi = 3.14f;
-        double height = 6.2;
-        char favLetter = 'n';
+        boolean isMale = true;                  //1 bit     
+        byte age = 24;                          //1 byte    -128 to 127
+        short salary = 32000;                   //2 byte    -2^15 to 2^15-1
+        int Age = 24;                           //4 byte    -2^31 to 2^31-1
+        long Salary = 35000;                    //8 byte    -2^63 to 2^63-1
+        float pi = 3.14f;                       //4 byte    
+        double height = 6.2;                    //8 byte
+        char favLetter = 'n';                   //2 byte
         System.out.println("Your name : "+name);
         System.out.println("Are you male : "+isMale);
         System.out.println("Your age : "+age);
