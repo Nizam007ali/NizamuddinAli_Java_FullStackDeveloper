1a20f869559aaa60cec3f9791d1803652729a12d
nizam007ali <nizam007ali@gmail.com>
2024-08-04 13:40:08 +0530

    ScopeOfVariable

---
+++ java/ScopeOfVariable.java
@@ -0,0 +1,13 @@
+public class scope_of_variable { 
+{  
+    static int myAge=24;//static (global) variable  
+    void method()  
+    {    
+        int favNum=90;//local variable    
+    }  
+    public static void main(String args[])  
+    {  
+        int born=1999;//instance variable    
+    }  
+} 
+
