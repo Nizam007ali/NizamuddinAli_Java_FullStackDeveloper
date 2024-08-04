b393b719ebe752152aef96224662d076a26119fc
nizam007ali <nizam007ali@gmail.com>
2024-08-04 14:59:30 +0530

    Primitive datatype + string

---
+++ java/PrimitiveDatatype.java
@@ -0,0 +1,23 @@
+public class PrimitiveDatatype {
+    public static void main(String args[])  
+    {  
+        String name = "Nizamuddin Ali";         //non-primitive
+        boolean isMale = true;
+        byte age = 24;
+        short salary = 32000;
+        int Age = 24;
+        long Salary = 35000;
+        float pi = 3.14f;
+        double height = 6.2;
+        char favLetter = 'n';
+        System.out.println("Your name : "+name);
+        System.out.println("Are you male : "+isMale);
+        System.out.println("Your age : "+age);
+        System.out.println("your salary : "+salary);
+        System.out.println("Your Age : "+Age);
+        System.out.println("your Salary : "+Salary);
+        System.out.println("pi value : "+pi);
+        System.out.println("Your height in foot : "+height);
+        System.out.println("Your favourite letter : "+favLetter);
+    }
+}
