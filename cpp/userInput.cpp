cf2b0e197b3f29179741dab7b59b99b49510bf76
nizam007ali <nizam007ali@gmail.com>
2024-08-11 14:46:39 +0530

    userInput

---
+++ cpp/userInput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name, email;
+    int id;
+
+    cout<<"Enter your Id : ";
+    cin>>id;                    //taking input
+    cout<<"Enter your name : ";
+    cin>>name;
+    cout<<"Enter your e-mail : ";
+    cin>>email;
+    
+    cout<<"Id : "<<id<<endl;
+    cout<<"Name : "<<name<<endl;
+    cout<<"E-mail : "<<email<<endl;
+
+    return 0;
+}
