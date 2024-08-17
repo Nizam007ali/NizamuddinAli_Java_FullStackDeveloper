c98a14b5443c859d733a26d16604972d8d4f4521
nizam007ali <nizam007ali@gmail.com>
2024-08-17 15:44:28 +0530

    arithmetic operator

---
+++ java/ArithmeticOperator.java
@@ -0,0 +1,13 @@
+public class ArithmeticOperator {
+    public static void main(String args[])  
+    { 
+        int a=25;  
+        int b=6;  
+
+        System.out.println(a+b);//31  
+        System.out.println(a-b);//19  
+        System.out.println(a*b);//150  
+        System.out.println(a/b);//4 
+        System.out.println(a%b);//1 
+    }
+}
