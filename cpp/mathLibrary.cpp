4d2ab62f1ccf47f519f5c7f7a4d8ea3bf122ddb3
nizam007ali <nizam007ali@gmail.com>
2024-08-17 15:24:20 +0530

    math library

---
+++ cpp/mathLibrary.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	
+    cout<<max(5,9)<<endl;		//only two parameter show max value
+    cout<<min(5,9)<<endl;		//only two parameter show min value
+    
+    //cmath library function
+    cout<<sqrt(169)<<endl;	//13 squareroot
+    cout<<round(2.4)<<endl;	//0.01-0.49 = 0, 0.5-0.99 = 1.
+    cout<<log(10)<<endl;		//log 
+    
+}
