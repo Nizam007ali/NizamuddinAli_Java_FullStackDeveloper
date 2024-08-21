d41dd49f72df6bee2524e81575be484ce7126e70
nizam007ali <nizam007ali@gmail.com>
2024-08-21 23:55:33 +0530

    math library

--- cpp/mathLibrary.cpp
+++ cpp/mathLibrary.cpp
@@ -11,6 +11,6 @@ int main()
     //cmath library function
     cout<<sqrt(169)<<endl;	//13 squareroot
     cout<<round(2.4)<<endl;	//0.01-0.49 = 0, 0.5-0.99 = 1.
-    cout<<log(10)<<endl;		//log 
+    cout<<log(10)<<endl;	//log 
     
 }
