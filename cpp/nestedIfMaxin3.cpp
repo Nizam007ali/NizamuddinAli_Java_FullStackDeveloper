9fc6389ee0a696b47587cf6fdb3bce0bbcdf393d
nizam007ali <nizam007ali@gmail.com>
2024-08-22 00:29:25 +0530

    nested if

---
+++ cpp/nestedIfMaxin3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int a, b, c, d;
+	cout<<"Maximum in three numbers."<<endl;
+	cout<<"First number : ";
+	cin>>a;
+	cout<<"Second number : ";
+	cin>>b;
+	cout<<"Third number : ";
+	cin>>c;
+	if(a>b){
+		if(a>c){
+			cout<<"max is "<<a<<endl;	
+		}
+		else{
+			cout<<"max is "<<c<<endl;	
+		}
+	}
+	else{
+		if(b>c)
+			cout<<"max is "<<b<<endl;
+		else
+			cout<<"max is "<<c<<endl;
+	}
+}
+
