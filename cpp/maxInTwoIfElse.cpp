331f21df7e76a2465963656db080dd9c7a69efad
nizam007ali <nizam007ali@gmail.com>
2024-08-22 00:13:09 +0530

    if else (maxin2)

---
+++ cpp/maxInTwoIfElse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int x, y;
+	cout<<"Maximum in two numbers."<<endl;
+	cout<<"First number : ";
+	cin>>x;
+	cout<<"Second number : ";
+	cin>>y;
+	if(x>y){
+		cout<<"max is "<<x<<endl;
+	}
+	else{
+		cout<<"max is "<<y<<endl;
+	}
+}
+
