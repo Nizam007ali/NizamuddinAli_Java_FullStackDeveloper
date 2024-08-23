96d7bebb8df5a7023123c828e01345e17ccf0c9b
nizam007ali <nizam007ali@gmail.com>
2024-08-23 12:19:15 +0530

    multiple variables

---
+++ python/multipleAssignment.py
@@ -0,0 +1,11 @@
+x, y = "Nizam", "Ali"   # multiple assignment different value
+print(x,y)              # if we use + it concatenate thus no space there, but when , used space has come in between
+
+a1 = a2 = a3 = 6        # multiple assignment same value
+print(a1*a2*a3)
+
+l = ["nizam","ali",24]  
+print(l)
+fn, ln, a = l           #unpack list 
+print("name :",fn,ln)
+print("age : ",a)
