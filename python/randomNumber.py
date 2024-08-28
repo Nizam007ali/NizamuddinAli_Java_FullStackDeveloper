b6f59b952a636f7e15970b2d2e92a93e6ea7902b
nizam007ali <nizam007ali@gmail.com>
2024-08-28 14:15:31 +0530

    random number

---
+++ python/randomNumber.py
@@ -0,0 +1,6 @@
+# Python does not have a random() function to make a random number, but Python has a built-in
+# module called random that can be used to make random numbers:
+
+import random                       # so we need to import random module in our code
+
+print(random.randrange(1, 7))       # gives the value between 1 to 6
