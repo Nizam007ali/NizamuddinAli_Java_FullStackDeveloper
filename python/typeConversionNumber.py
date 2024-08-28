eaf64e40c9dca89af33231723cdecfdc40295b0f
nizam007ali <nizam007ali@gmail.com>
2024-08-28 14:16:00 +0530

    type conversion

--- python/typeConversionNumber.py
+++ python/typeConversionNumber.py
@@ -1,18 +1,34 @@
 x = 5               # int
 y = 6.2             # float
 z = 3 + 9j          # complex
+s = "4"             # string
 
 a = float(x)        #convert from int to float:
 b = int(y)          #convert from float to int:
 c = complex(x)      #convert from int to complex:
-d = complex(y)
+d = complex(y)      #convert from floatt to complex:
+e = int(s)          #convert from string to int:
+f = float(s)        #convert from string to float:
+g = complex(s)      #convert from string to complex:
+h = str(x)          #convert from int to string:
+i = str(z)          #convert from complex to string:
 
 print(a)
 print(b)
 print(c)
 print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
 
 print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
