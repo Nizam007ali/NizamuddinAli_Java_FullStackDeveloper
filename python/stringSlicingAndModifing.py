f32006d297fae558edefb9de40491adf21ac6275
nizam007ali <nizam007ali@gmail.com>
2024-08-28 14:49:35 +0530

    string slycing modifying

---
+++ python/stringSlicingAndModifing.py
@@ -0,0 +1,15 @@
+a = "Hello, World!"         # index start from 0
+print(a[7:12])              # print 7 to 11 index
+print(a[7:])                # print 7 to last index
+print(a[:5])                # print start to 4 index
+print(a[-6:-1])             # negative indexing -6 is w and goes to -2 d
+
+#modifying
+print(a.upper())            # change to uppercase
+print(a.lower())            # change to lowercase
+print(a.replace("H", "Y"))  # change H to Y 
+print(a.split(","))         # Split the line before and after of ,
+
+b = " Hello "
+print(b.strip())            # remove the unwanted whitespace fisrt and last
+
