cd3dd50dfde8af44f737cb76eec690645d5626b1
nizam007ali <nizam007ali@gmail.com>
2024-08-28 14:30:24 +0530

    string

---
+++ python/string.py
@@ -0,0 +1,19 @@
+str1 = 'hello'
+str2 = "hello"          #both contain same string
+print(str1==str2)
+
+# we can use quotes but make sure it doesn't match the quotes surrounding the string
+print("Hello! It's Nizam")
+print("He is called 'Haider'")
+print('He is called "Nitish"')
+
+#Multiline string we can use and tabspace also in it
+str = '''   Johny, Johny
+    (Yes, papa)
+    Eating sugar?
+    (No, papa)
+    Telling lies?
+    (No, papa)
+    Open your mouth
+    (Ah, ah, ah)'''
+print(str)
