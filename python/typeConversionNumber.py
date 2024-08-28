2bf5c24f95772b48a4ead3b4f4b35d241fb765a2
nizam007ali <nizam007ali@gmail.com>
2024-08-28 14:01:39 +0530

    type conversion in numbers

---
+++ python/typeConversionNumber.py
@@ -0,0 +1,18 @@
+x = 5               # int
+y = 6.2             # float
+z = 3 + 9j          # complex
+
+a = float(x)        #convert from int to float:
+b = int(y)          #convert from float to int:
+c = complex(x)      #convert from int to complex:
+d = complex(y)
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
