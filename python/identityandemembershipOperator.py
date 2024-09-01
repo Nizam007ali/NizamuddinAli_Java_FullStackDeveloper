ba7a7bf03726fc53c349ad13f2b268741885a7d0
nizam007ali <nizam007ali@gmail.com>
2024-09-01 09:36:31 +0530

    identity and membership operator

---
+++ python/identityandemembershipOperator.py
@@ -0,0 +1,20 @@
+# identity operator
+
+x = ["hello", "world"]
+y = ["hello", "world"]
+z = x
+
+# returns True because z is the same object as x
+print(x is z)
+
+# returns False because x is not the same object as y, even if they have the same content
+print(x is not y)
+
+# to demonstrate the difference betweeen "is" and "==": this comparison returns True because x is equal to y
+print(x == y)
+
+# membership operator
+print("hello" in x)
+print("mango" not in x)
+
+
