660c9e7362a8038b151687e77f3a67daba670ce3
nizam007ali <nizam007ali@gmail.com>
2024-09-01 09:09:38 +0530

    aritjmetic operator

---
+++ python/arithmeticOperator.py
@@ -0,0 +1,27 @@
+x = 19
+y = 4
+
+# print values
+print("x : ",x)
+print("y : ",y)
+
+# addition
+print("x + y = ", x+y)
+
+# substraction
+print("x - y = ", x-y)
+
+# multilication
+print("x * y = ", x*y)
+
+# division
+print("x / y = ", x/y)
+
+# modulous
+print("x % y = ", x%y)
+
+# exponentiaition
+print("x ** y = ", x**y)
+
+# floor division
+print("x // y = ", x//y)
