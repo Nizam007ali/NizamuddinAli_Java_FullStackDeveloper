cf5fe6e4c44fae09d9c7c73449914f7f5c04543b
nizam007ali <nizam007ali@gmail.com>
2024-09-01 09:27:27 +0530

    assignment operator

---
+++ python/assignmentOperator.py
@@ -0,0 +1,56 @@
+# assignment 
+x = 9
+print(x)
+
+# assignment and addition
+x += 5
+print(x)
+
+# assignment and substracion
+x -= 5
+print(x)
+
+# assignment and multiplication
+x *= 5
+print(x)
+
+# assignment and division
+x /= 5
+print(x)
+
+# assignment and modulous
+x %= 5
+print(x)
+
+x=20
+# assignment and floor division
+x //= 5
+print(x)
+
+# assignment and exponentiation
+x **= 2
+print(x)
+
+# assignment and 'and'
+x &= 5
+print(x)
+
+# assignment and or
+x |= 9
+print(x)
+
+# assignment and xor
+x ^= 7
+print(x)
+
+# assignment and left shift
+x >>= 2
+print(x)
+
+# assignment and right shift
+x <<= 2
+print(x)
+
+# assignment and print
+print(x := 5)
+
