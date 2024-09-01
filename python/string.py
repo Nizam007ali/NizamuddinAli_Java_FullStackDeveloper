7c3e850afdad5578584a2af767eff3293f57e68a
nizam007ali <nizam007ali@gmail.com>
2024-09-01 08:49:49 +0530

    string methods

--- python/string.py
+++ python/string.py
@@ -7,6 +7,16 @@ print("Hello! It's Nizam")
 print("He is called 'Haider'")
 print('He is called "Nitish"')
 
+#escape characters
+print('It\'s ok')                               # \'	Single Quote	
+print('6\\2 = ',(6/2))                          # \\	Backslash	
+print('My name is nizam \n Welcome back!')      # \n	New Line	
+print('My name is nizam \r Welcome back!')      # \r	Carriage Return	
+print('My name is nizam, \t Welcome back!')     # \t	Tab	
+print('My name is nizam, \bWelcome back!')      # \b	Backspace
+print("\110\145\154\154\157")                   # \ooo	Octal value	
+print("\x48\x65\x6c\x6c\x6f")                   # \xhh	Hex value
+
 #Multiline string
 str = '''   Johny, Johny
     (Yes, papa)
