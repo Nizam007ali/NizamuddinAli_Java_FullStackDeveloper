6c21bbe8589ad17e06d81ec7e89669d4b8cffb7d
nizam007ali <nizam007ali@gmail.com>
2024-09-04 13:37:12 +0530

    list tems change

---
+++ python/listItemChange.py
@@ -0,0 +1,13 @@
+subject = ["physics", "chemistry",  "english"]                      # list iitialistation
+print(subject)
+print()
+
+print("_____________________________________________________________")
+print("change values")
+subject[1] = "mathematics"                                          # change 1 item index 1
+print(subject)
+subject[1:3] = ["chemistry"]                                        # change 2 with one item index 1,2
+print(subject)
+subject[1:3] = ["chemistry", "hindi"]                               # change 2 with two item index 1,2
+print(subject)
+print()
