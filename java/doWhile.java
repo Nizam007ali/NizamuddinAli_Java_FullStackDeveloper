0e59712e9675dcf4513f9a3c7ad459192b2d1a7a
nizam007ali <nizam007ali@gmail.com>
2024-09-07 11:20:24 +0530

    do while

---
+++ java/doWhile.java
@@ -0,0 +1,16 @@
+package git;
+public class doWhile {
+    public static void main(String str[]){
+        int i = 1;
+        
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+        
+        do{         //thid shows whwther condition is true or false it will ru once at a time
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+    }
+}
