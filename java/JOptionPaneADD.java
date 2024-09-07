e7d978a76bad5b436d77f87f7a4a38508547c941
nizam007ali <nizam007ali@gmail.com>
2024-09-07 14:26:09 +0530

    Joptionpane

---
+++ java/JOptionPaneADD.java
@@ -0,0 +1,23 @@
+package git;
+import javax.swing.JOptionPane;
+
+public class jOptionPaneAdd {
+
+    public static void main(String str[]) {
+        int a = 0, b, d;
+        while (true) {
+            b = Integer.parseInt(JOptionPane.showInputDialog("enter number"));
+            a = a + b;
+            JOptionPane.showMessageDialog(null, ("Sum = " + a));
+            d = JOptionPane.showConfirmDialog(null, "do you want to sum another value");
+            if (d == 0) {
+            } else if (d == 1) {
+            JOptionPane.showMessageDialog(null, ("Final Sum = " + a));
+                break;
+            } else {
+                JOptionPane.showMessageDialog(null, "wrong choice...");
+                break;
+            }
+        }
+    }
+}
