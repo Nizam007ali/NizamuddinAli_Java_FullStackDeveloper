e3f88defd03ad1aca5b236cc96189435642d1a03
nizam007ali <nizam007ali@gmail.com>
2024-09-07 16:27:02 +0530

    while even

---
+++ java/whileEven0to20.java
@@ -0,0 +1,11 @@
+public class whileEven0to20 {
+    public static void main(String str[]){
+        int i = 1;
+        while(i <= 20){
+            if(i % 2 == 0){
+                System.out.print(i+" ");
+            }
+            i++;
+        }
+    }
+}
