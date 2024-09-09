8115eca51b7d2de1e3a03f6a97930f1bcd961b71
nizam007ali <nizam007ali@gmail.com>
2024-09-09 10:48:02 +0530

    function with reference

---
+++ cpp/funWithReference.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int cal(int &x, int &y, char &z){
+	if(z=='+')
+		return x+y;
+	else if(z=='-')
+		return x-y;
+	else if(z=='*')
+		return x*y;
+	if(z=='/')
+		return x/y;
+	else
+		return 0;
+}
+
+int main()
+{
+	int a, b;
+	char c;
+	cout<<"Enter 1 number : ";
+	cin>>a;
+	cout<<"Enter 2 number : ";
+	cin>>b;
+	cout<<"Enter either(+,-,*,/) number : ";
+	cin>>c;
+	cout<<a<<" "<<c<<" "<<b<<" = "<<cal(a,b,c);
+}
+
