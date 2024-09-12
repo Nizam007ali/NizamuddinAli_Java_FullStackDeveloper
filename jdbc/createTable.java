c68a81035c6ea32b96ab00d916b07f10f40e5632
nizam007ali <nizam007ali@gmail.com>
2024-09-13 00:48:18 +0530

    create and delete table

---
+++ jdbc/createTable.java
@@ -0,0 +1,19 @@
+package jdbc;
+import java.sql.*;
+
+public class createTable {
+    public static void main(String str[]) throws ClassNotFoundException, SQLException {
+        
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cyindb","root","1234");
+        
+        Statement stmt = conn.createStatement();
+        
+        stmt.executeUpdate("create table student(id int, name varchar(20));");
+        System.out.println("table created");
+        stmt.executeUpdate("drop table student;");
+        System.out.println("table deleted");
+        
+    }
+}
