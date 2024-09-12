66e3bab8d646399921d0661387cfbef93e9962ce
nizam007ali <nizam007ali@gmail.com>
2024-09-13 00:50:48 +0530

    create and delete database

--- jdbc/createDatabase.java
+++ jdbc/createDatabase.java
@@ -4,21 +4,27 @@ import java.sql.*;
 
 public class createDatabase {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        
+
         Class.forName("com.mysql.cj.jdbc.Driver");
-        
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/","root","1234");
-        
+
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "1234");
+
         Statement stmt = conn.createStatement();
-        
+
         int r;
         r = stmt.executeUpdate("create database git;");
-        if(r>0) System.out.println("Database created.");
-        else System.out.println("Database not created.");
-        
+        if (r > 0)
+            System.out.println("Database created.");
+        else
+            System.out.println("Database not created.");
+
         r = stmt.executeUpdate("drop database git");
-        if(r==0) System.out.println("Database deleted.");
-        else System.out.println("Database not deleted.");
-        
+        if (r == 0)
+            System.out.println("Database deleted.");
+        else
+            System.out.println("Database not deleted.");
+
+        conn.close();
+
     }
 }
