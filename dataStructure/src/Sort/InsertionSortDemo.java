f93bc615b29b1f981e767e9d2d11358b13a42b25
nizam007ali <nizam007ali@gmail.com>
2024-11-21 11:56:05 +0530

    insertion sort

--- dataStructure/src/Sort/InsertionSortDemo.java
+++ dataStructure/src/Sort/InsertionSortDemo.java
@@ -1,41 +1,43 @@
+package Sort;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Sort;
-
 /**
  *
  * @author nizam
  */
-public class SelectionSortDemo {
+public class InsertionSortDemo {
 
     public static void Display(int arr[]) {
-        System.out.println("Array Elements : ");
         for (int i = 0; i < arr.length; i++) {
             System.out.print("-->" + arr[i]);
         }
         System.out.println();
     }
 
-    public static void SelectionSort(int arr[]) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            int min = i;
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[j] < arr[min]) {
-                    min = j;
-                }
+    public static void insertionSort(int arr[]) {
+        int i, j;
+        for (i = 0; i < arr.length; i++) {
+            int key = arr[i];
+            j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
             }
-            int c = arr[i];
-            arr[i] = arr[min];
-            arr[min] = c;
+            arr[j + 1] = key;
+            System.out.println("Step " + (i + 1) + " : ");
+            Display(arr);
         }
     }
 
     public static void main(String[] args) {
         int arr[] = {3, 7, 8, 5, 1, 6, 2, 9, 4, 0};
+        System.out.println("Array Elements before sorting : ");
         Display(arr);
-        SelectionSort(arr);
+        insertionSort(arr);
+        System.out.println("Array Elements after sorting : ");
         Display(arr);
     }
 
