a2c0f58764d04f19b4df81677ad0109444f71c75
nizam007ali <nizam007ali@gmail.com>
2024-11-24 19:27:12 +0530

    employee jdbc

---
+++ empjdbc/src/main/java/com/mycompany/empjdbc/Empjdbc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.empjdbc;
+
+/**
+ *
+ * @author nizam
+ */
+public class Empjdbc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
