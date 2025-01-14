c956d8c81b396f3405897293fe6465b4fb3c6ca7
nizam007ali <nizam007ali@gmail.com>
2025-01-15 01:01:17 +0530

    only if max or equal in two

---
+++ JavaApplication/src/javaapplication/J025OnlyIfMaxorEqualinTwo.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nizam
+ */
+public class J025OnlyIfMaxorEqualinTwo {
+    public static void main(String[] args) {
+        String res = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Max or Equal in Two numbers.");
+        System.out.print("Enter First number : ");
+        int n1 = sc.nextInt();
+        System.out.print("Enter Second number : ");
+        int n2 = sc.nextInt();
+        System.out.println("==================================================");
+        System.out.println("First number " + n1 + ".");
+        System.out.println("Second number " + n2 + ".");
+        if(n1>n2) {
+            System.out.println("First if executed");
+            System.out.println("Number "+n1+" is greater than "+n2+".");
+        }
+        if(n1<n2) {
+            System.out.println("Second if executed");
+            System.out.println("Number "+n2+" is greater than "+n1+".");
+        }
+        if(n1==n2) {
+            System.out.println("Third if executed");
+            System.out.println("Both numbers are equals");
+        }
+        System.out.println(res);
+        System.out.println("==================================================");
+    }
+}
