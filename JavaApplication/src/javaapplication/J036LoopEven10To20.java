bb185f9c45b33308f7e477c97eb42ac61f06d6dc
nizam007ali <nizam007ali@gmail.com>
2025-01-19 08:55:51 +0530

    loop even 10 to 20

---
+++ JavaApplication/src/javaapplication/J036LoopEven10To20.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nizam
+ */
+public class J036LoopEven10To20 {
+    public static void main(String[] args) {
+        System.out.println("==================================================");
+        System.out.println("===================Even 10 to 20==================");
+        System.out.println("==================================================");
+        for(int i=10; i<=20;i=i+2) {
+            System.out.print(i+" ");
+        }
+        System.out.println("");
+        System.out.println("==================================================");
+    }
+}
