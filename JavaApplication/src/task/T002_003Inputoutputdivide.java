817b0672906086adcb221b5d7566c502a0c350a9
nizam007ali <nizam007ali@gmail.com>
2025-01-20 00:17:10 +0530

    divide

---
+++ JavaApplication/src/task/T002_003Inputoutputdivide.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package task;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nizam
+ */
+public class T002_003Inputoutputdivide {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("==================================================");
+        System.out.println("========================Add=======================");
+        System.out.println("==================================================");
+        System.out.print("Enter first number : ");
+        int n1 = sc.nextInt();
+        System.out.print("Enter second number : ");
+        int n2 = sc.nextInt();
+        System.out.println("==================================================");
+        System.out.println(n1+" + "+n2+" = "+(n1+n2));
+        System.out.println("==================================================");
+    }
+}
