826d71d7a1b0f2f9f2b1288bf95ef6c998b743cf
nizam007ali <nizam007ali@gmail.com>
2025-01-20 00:19:18 +0530

    Math

---
+++ JavaApplication/src/task/T002_006InputoutputMath.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package task;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nizam
+ */
+public class T002_006InputoutputMath {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("==================================================");
+        System.out.println("========================Add=======================");
+        System.out.println("==================================================");
+        System.out.print("Enter first number : ");
+        int n1 = sc.nextInt();
+        System.out.print("Enter second number : ");
+        int n2 = sc.nextInt();
+        System.out.println("==================================================");
+        System.out.println(n1+" + "+n2+" = "+(n1+n2));
+        System.out.println(n1+" - "+n2+" = "+(n1-n2));
+        System.out.println(n1+" * "+n2+" = "+(n1*n2));
+        System.out.println(n1+" / "+n2+" = "+(n1/n2));
+        System.out.println(n1+" % "+n2+" = "+(n1%n2));
+        System.out.println("==================================================");
+    }
+}
