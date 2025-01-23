8c3292e72402af5e8441e49bf5f7c486e82430cb
nizam007ali <nizam007ali@gmail.com>
2025-01-23 13:24:16 +0530

    system exit

---
+++ JavaApplication/src/javaapplication/J051SystemExit.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nizam
+ */
+public class J051SystemExit {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("==================================================");
+        System.out.println("===================System.exit====================");
+        System.out.println("==================================================");
+        System.out.println("Hello");
+        System.exit(0);
+        System.out.println("Nizam");
+        System.out.println("==================================================");
+    }
+}
