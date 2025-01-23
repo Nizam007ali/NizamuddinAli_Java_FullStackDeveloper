be32be738526d7c81dcb1ac064b2ae7a141bcae9
nizam007ali <nizam007ali@gmail.com>
2025-01-23 13:25:02 +0530

    pattern

---
+++ JavaApplication/src/javaapplication/J054LoopPattern3.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nizam
+ */
+public class J054LoopPattern3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=================================================");
+        System.out.println("================  *              ================");
+        System.out.println("================  *  *           ================");
+        System.out.println("================  *  *  *        ================");
+        System.out.println("================  *  *  *  *     ================");
+        System.out.println("================  *  *  *  *  *  ================");
+        System.out.println("=================================================");
+        int r = 5;
+        int c = 5;
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= c; j++) {
+                if(j<=i)
+                System.out.print("  *");
+                if(j==c) System.out.println("  ");
+            }
+        }
+        System.out.println("==================================================");
+    }
+}
