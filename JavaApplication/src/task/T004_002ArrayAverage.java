8c64e57bbaa5238524a120c1253d966007d86f5e
nizam007ali <nizam007ali@gmail.com>
2025-01-23 16:48:35 +0530

    array average

---
+++ JavaApplication/src/task/T004_002ArrayAverage.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package task;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nizam
+ */
+public class T004_002ArrayAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("===================================================");
+        System.out.println("================== Array Average ==================");
+        System.out.println("===================================================");
+        System.out.print("Enter array size : ");
+        int n=sc.nextInt(),sum=0;
+        int arr[] = new int[n];
+        for(int i=0; i<arr.length; i++) {
+            System.out.print("Enter "+(i+1)+" value : ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Array : ");
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(" "+arr[i]);
+            sum+=arr[i];
+        }
+        System.out.println("");
+        System.out.println("Average : "+((float)sum/n));
+        System.out.println("===================================================");
+    }
+}
