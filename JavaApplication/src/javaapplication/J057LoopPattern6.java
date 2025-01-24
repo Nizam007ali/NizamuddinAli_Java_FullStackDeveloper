1756821918bac583abe25e9a4d025d1817e56bf0
nizam007ali <nizam007ali@gmail.com>
2025-01-24 12:04:24 +0530

    array pattern

---
+++ JavaApplication/src/javaapplication/J057LoopPattern6.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nizam
+ */
+public class J057LoopPattern6 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=================================================");
+        System.out.println("================  1  2  3  4  5  ================");
+        System.out.println("================  1  2  3  4     ================");
+        System.out.println("================  1  2  3        ================");
+        System.out.println("================  1  2           ================");
+        System.out.println("================  1              ================");
+        System.out.println("=================================================");
+        int r = 5, c = 5, n = 5;
+        // 1 5 //2 4 //3 3 //4 2//1 5
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= c; j++) {
+                if(j+i<7) System.out.print("  "+j);
+                else System.out.print("   ");
+                if(j==c) System.out.println("  ");
+            }
+            n--;
+        }
+        System.out.println("==================================================");
+    }
+}
