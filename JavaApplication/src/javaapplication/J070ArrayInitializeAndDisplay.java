1be590ab75c38a553052804248ca23cd9a669ceb
nizam007ali <nizam007ali@gmail.com>
2025-01-25 11:02:01 +0530

    Array

---
+++ JavaApplication/src/javaapplication/J070ArrayInitializeAndDisplay.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nizam
+ */
+public class J070ArrayInitializeAndDisplay {
+
+    public static void main(String[] args) {
+        System.out.println("==================================================");
+        System.out.println("========== Array initialize and display ==========");
+        System.out.println("==================================================");
+        int arr[] = new int[5];     // initalizon without value only length needed
+        System.out.println("Length : "+arr.length);
+        System.out.println("arr[0] : "+arr[0]);
+        System.out.println("arr[1] : "+arr[1]);
+        System.out.println("arr[2] : "+arr[2]);
+        System.out.println("arr[3] : "+arr[3]);
+        System.out.println("arr[4] : "+arr[4]);
+        System.out.println("==================================================");
+    }
+}
