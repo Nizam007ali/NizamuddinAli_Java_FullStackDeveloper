d5f022c92af78fd0727f6cff09b421756bb12d6c
nizam007ali <nizam007ali@gmail.com>
2025-01-25 10:59:44 +0530

    loop pattern

---
+++ JavaApplication/src/javaapplication/J063LoopPattern12.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nizam
+ */
+public class J063LoopPattern12 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=================================================");
+        System.out.println("================  0              ================");
+        System.out.println("================  1  0           ================");
+        System.out.println("================  0  1  0        ================");
+        System.out.println("================  1  0  1  0     ================");
+        System.out.println("================  0  1  0  1  0  ================");
+        System.out.println("=================================================");
+        int r = 5, c = 5, n=1;
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= c; j++) {
+                n=(n==1)?0:1;
+                if(j<=i) System.out.print("  "+n);
+                else System.out.print("   ");
+                if(j==c) System.out.println("  ");
+            }
+        }
+        System.out.println("==================================================");
+    }
+}
