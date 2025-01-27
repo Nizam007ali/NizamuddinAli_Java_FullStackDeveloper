6f6ffa13c610242e33c606bf653b2f70eeef366b
nizam007ali <nizam007ali@gmail.com>
2025-01-27 22:51:30 +0530

    array with value

---
+++ JavaApplication/src/javaapplication/J071ArrayInitializeWithValueAndDisplay.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nizam
+ */
+public class J071ArrayInitializeWithValueAndDisplay {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("===================================================");
+        System.out.println("===== Array initialize with value and display =====");
+        System.out.println("===================================================");
+        int arr[] = {10,20,30,40,50};     // initalizon without value only length needed
+        System.out.println("Length : "+arr.length);
+        System.out.println("arr[0] : "+arr[0]);
+        System.out.println("arr[1] : "+arr[1]);
+        System.out.println("arr[2] : "+arr[2]);
+        System.out.println("arr[3] : "+arr[3]);
+        System.out.println("arr[4] : "+arr[4]);
+        System.out.println("==================================================");
+    }
+}
