e3af7b8946462c5b0f37686d3338c46b32550493
nizam007ali <nizam007ali@gmail.com>
2025-01-30 10:48:25 +0530

    loop neg 5 to 5

---
+++ JavaApplication/src/task/T004_ht001LoopNeg5To5.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package task;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nizam
+ */
+public class T004_ht001LoopNeg5To5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("==================================================");
+        System.out.println("=============== Loop print -5 to 5 ===============");
+        System.out.println("==================================================");
+        for(int i=-5; i<=5; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println("");
+        System.out.println("==================================================");
+    }
+}
